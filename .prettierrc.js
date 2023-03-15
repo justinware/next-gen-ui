09133b240260d77f33be550d8ce22ff76c7640ae
Justin Ware <wareisjustin@gmail.com>
2023-03-16 06:30:22 +1000

    Added Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  bracketSameLine: false,
+};
